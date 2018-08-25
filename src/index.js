832f8e13b7933464ff3baae5a22bfc05783ce9be
gredfearn <grantredfearn@gmail.com>
2018-08-25 16:37:22 -0500

    added config for amplify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import amplify from './utils/amplify';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+// init AWS Amplify
+amplify();
 
 ReactDOM.render(
   <Router>
